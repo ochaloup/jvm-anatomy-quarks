6f948b8a2f1746d0bfdaef0b399e983d8e650b23
Ondra Chaloupka <ochaloup@redhat.com>
2020-10-09 11:36:27 +0200

    just a try to print some assembly stuff

---
+++ assembly-print/src/main/java/cz/chalda/App.java
@@ -0,0 +1,15 @@
+package cz.chalda;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    private static int i;
+
+    public static void main( String[] args )
+    {
+        App.i = 42;
+    }
+}
